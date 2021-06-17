b325fb7f4dbf5055a15065c9e753f22f3d15efff
victerryso <victerry.so@gmail.com>
2021-06-18 00:52:10 +1000

    added lambda code and request data fn

---
+++ src/lib/lambda-code.js
@@ -0,0 +1,46 @@
+const https = require("https");
+
+const key = "xxx";
+
+exports.handler = async (event, context) => {
+  const params = event.rawQueryString;
+
+  const options = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `apikey ${key}`,
+    },
+    hostname: "api.transport.nsw.gov.au",
+    port: 443,
+    path: `/v1/tp/add_info?outputFormat=rapidJSON&filterDateValid=01-10-2016&version=10.2.1.42&`,
+    method: "GET",
+  };
+
+  const promise = new Promise((resolve, reject) => {
+    https
+      .get(options, (response) => {
+        let json = "";
+
+        response.on("data", (chunk) => {
+          json += chunk;
+        });
+
+        response.on("end", () => {
+          resolve({
+            statusCode: 200,
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify(json),
+          });
+        });
+      })
+      .on("error", (e) => {
+        reject(Error(e));
+      });
+  });
+
+  return promise;
+};
---
+++ src/lib/request-data.ts
@@ -0,0 +1,10 @@
+const url = "https://5ily0l2l1l.execute-api.ap-southeast-2.amazonaws.com/tfnsw";
+
+const requestData = async (params = {}) => {
+  const response = await fetch(url);
+  const json = await response.json();
+
+  return json;
+};
+
+export default requestData;
